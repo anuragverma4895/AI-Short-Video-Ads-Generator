cfa467a54ae95010efc278e5a98f878dd942f0cd
Anurag Verma <anuragverma4895@users.noreply.github.com>
2026-04-05 19:03:45 +0530

    fixed deployment erorr

---
+++ server/configs/instrument.ts
@@ -0,0 +1,8 @@
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN || "https://4ae386d8f2b0bc6792263ea95c2a2367@o4511053052444672.ingest.us.sentry.io/4511053058080768",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
